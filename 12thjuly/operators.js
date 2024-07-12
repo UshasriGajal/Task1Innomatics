a2224d35fd76e526ebd26d2fca69ed2012f11028
UshasriGajal <ushasrigajal@gmail.com>
2024-07-12 16:29:07 +0530

    12th july

---
+++ 12thjuly/operators.js
@@ -0,0 +1,28 @@
+let x = 10
+let y = 20
+// console.log("add",x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x % y)
+// console.log(x / y)
+// console.log(x ** y)
+// console.log(x = y)
+// console.log(x < y)
+// console.log(x > y)
+// console.log(x == y)
+// console.log(x === y)
+// console.log(x <= y)
+// console.log(x >= y)
+// // console.log(x++)
+// // console.log(x--)
+// console.log(x && y)
+// console.log(x || y)
+// console.log((x+y)*y)
+// console.log(x++)
+// console.log(y--)
+// console.log(10 == "1")
+// console.log(x = y ? true : false)
+let z = null
+console.log(z)
+
+
